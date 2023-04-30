314bf915802a7360e1e70cf391a33577d614bc9e
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-04-30 08:26:08 +0300

    add project layers and dbcontext and entities

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using Infrastrucure.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace API
 {
     public class Program
@@ -12,6 +15,11 @@ namespace API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<StoreContext>(opt =>
+            {
+                opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
---
+++ Infrastrucure/Data/StoreContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastrucure.Data
+{
+    public class StoreContextFactory : IDesignTimeDbContextFactory<StoreContext>
+    {
+        public StoreContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<StoreContext>();
+            optionsBuilder.UseSqlite("Data source=eccomerce1.db");
+            return new StoreContext(optionsBuilder.Options);
+        }
+    }
+}
